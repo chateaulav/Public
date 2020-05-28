47a53f09ecb6e64ebe828387dca3f16eb73d899b
Chateau-Lav <jonathan.g.race.mil@mail.mil>
2020-05-28 09:54:49 -0400

    added custom openstack code, and enhanced README

---
+++ OpenStack/nova/files/addons.js
@@ -0,0 +1,9 @@
+"use strict";
+
+function openNav() {
+    document.getElementById("Sidenav").style.width = "400px";
+}
+
+function closeNav() {
+    document.getElementById("Sidenav").style.width = "0";
+}
